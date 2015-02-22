616be111c93d253dd20a801ccf82adc74ec52304
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-22 18:53:16 +0100

    missing const tests added

--- jvm/consts_test.go
+++ jvm/consts_test.go
@@ -158,43 +158,91 @@ func TestStringNullLong(t *testing.T) {
 }
 
 func TestStringPositiveDouble(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_DOUBLE, value: []byte{0x3f, 0xf5, 0x1e, 0xb8,
+		0x51, 0xeb, 0x85, 0x1f}}
+
+	var res float64
+	err := cst.dumpValue(&res)
+
+	assert.Nil(t, err)
+	assert.Equal(t, float64(1.32), res)
+	assert.Equal(t, "Double(1.320000)", cst.String())
 }
 
 func TestStringNegativeDouble(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_DOUBLE, value: []byte{0xc0, 0x09, 0x21, 0xca, 0xc0,
+		0x83, 0x12, 0x6f}}
+
+	var res float64
+	err := cst.dumpValue(&res)
+
+	assert.Nil(t, err)
+	assert.Equal(t, float64(-3.1415), res)
+	assert.Equal(t, "Double(-3.141500)", cst.String())
 }
 
 func TestStringNullDouble(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_DOUBLE, value: []byte{0, 0, 0, 0, 0, 0, 0, 0}}
+
+	var res float64
+	err := cst.dumpValue(&res)
+
+	assert.Nil(t, err)
+	assert.Equal(t, float64(0), res)
+	assert.Equal(t, "Double(0.000000)", cst.String())
 }
 
 func TestStringClassRef(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_CLASS_REF, value: []byte{0, 42}}
+
+	var res int16
+	err := cst.dumpValue(&res)
+
+	assert.Nil(t, err)
+	assert.Equal(t, int16(42), res)
+	assert.Equal(t, "#42", cst.String())
 }
 
 func TestStringStringRef(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_STRING_REF, value: []byte{0, 42}}
+
+	var res int16
+	err := cst.dumpValue(&res)
+
+	assert.Nil(t, err)
+	assert.Equal(t, int16(42), res)
+	assert.Equal(t, "#42", cst.String())
 }
 
 func TestStringMethodType(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_METHOD_TYPE, value: []byte{0, 42}}
+
+	var res int16
+	err := cst.dumpValue(&res)
+
+	assert.Nil(t, err)
+	assert.Equal(t, int16(42), res)
+	assert.Equal(t, "#42", cst.String())
 }
 
 func TestStringFieldRef(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_FIELD_REF, value: []byte{0, 42, 0, 17}}
+	assert.Equal(t, "#42:#17", cst.String())
 }
 
 func TestStringMethodRef(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_METHOD_REF, value: []byte{0, 42, 0, 17}}
+	assert.Equal(t, "#42:#17", cst.String())
 }
 
 func TestStringInterfaceMethodRef(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_INTERFACE_METHOD_REF, value: []byte{0, 42, 0, 17}}
+	assert.Equal(t, "#42:#17", cst.String())
 }
 
 func TestStringNameType(t *testing.T) {
-	// TODO
+	cst := JConst{tag: TAG_NAME_TYPE_DESC, value: []byte{0, 42, 0, 17}}
+	assert.Equal(t, "#42:#17", cst.String())
 }
 
 func TestStringMethodHandle(t *testing.T) {
